d7258174f14625b3dddf4c8b4e2c16e2ee0d1db0
mahmad <immunir78@gmail.com>
2022-05-28 20:44:24 +0500

    adding/updating

--- Index.js
+++ Index.js
@@ -1,7 +1,8 @@
-var express = require("Express");
+var express = require('express');
 var app = express();
-app.get('/', function(req, res){    
-    res.send("Hello world, there");
 
+app.get('/hello/:id/:name', (req, res)=>{
+    res.send('The id is ' + req.params.id + ' and name is ' + req.params.name);
 });
+
 app.listen(8080);
